39e19cf95d9cd1d41e6029e501e1d3ae614ca1e6
giorgi <giorgigvazava87@gmail.com>
2023-07-21 16:30:22 +0400

    header done

--- _markup/index.php
+++ _markup/index.php
@@ -6,22 +6,82 @@ $version = "1.0.1";
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" href="/_markup/assets/img/favicon.svg" type="image/svg" sizes="32x32">
-	<title>ნიუ</title>
-	<link rel="stylesheet" type="text/css" href="/_markup/assets/bootstrap/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="/_markup/assets/owl/owl.carousel.min.css" />
-	<link rel="stylesheet" href="/_markup/assets/lightbox/jquery.lightbox.css" />
-	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/main.css?v=<?=$version?>" />
-	<link rel="stylesheet" type="text/css" href="/_markup/assets/aos/aos.css" />
-	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/ge.css?v=<?=$version?>" />
-	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/responsive_ge.css?v=<?=$version?>" />
+	<link rel="icon" href="/_markup/assets/img/favicon.svg" type="image/svg" sizes="64x64">
+	<title>Billion</title>
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/owl/owl.carousel.min.css">
+	<link rel="stylesheet" href="/_markup/assets/lightbox/jquery.lightbox.css">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/main.css?v=<?=$version?>">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/aos/aos.css">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/ge.css?v=<?=$version?>">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/responsive_ge.css?v=<?=$version?>">
 </head>
 <body>
 
-<section class="mobile-navigation"></section>
+<div class="mobile-navigation"></div>
 
 <header>
-	
+	<div class="top">
+		<div class="center">
+			<div class="content">
+				<ul class="social">
+					<li><a href="" class="facebook" aria-label="Facebook">facebook</a></li>
+					<li><a href="" class="instagram" aria-label="Instagram">instagram</a></li>
+					<li><a href="" class="youtube" aria-label="Youtube">youtube</a></li>
+				</ul>
+
+				<div class="line"></div>
+
+				<div class="phone">
+					<a href="" aria-label="Contact number" class="call">
+						<span>032 215 15 09</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="bottom">
+		<div class="center">
+			<a href="" class="logo" aria-label="Logo">Logo</a>
+
+			<div class="right">
+				<nav>
+					<ul>
+						<li><a href="" aria-label="მარკეტების რუკა" class="active">მარკეტების რუკა</a></li>
+						<li><a href="" aria-label="ფასდაკლებები">ფასდაკლებები</a></li>
+						<li>
+							<a href="" aria-label="პროდუქტები">პროდუქტები</a>
+							<ul>
+								<li><a href="" aria-label="ხილ-ბოსტნეული">ხილ-ბოსტნეული</a></li>
+								<li><a href="" aria-label="სასმელი">სასმელი</a></li>
+								<li><a href="" aria-label="ჩაი ყავა კაკაო">ჩაი ყავა კაკაო</a></li>
+								<li><a href="" aria-label="პურფუნთუშეული">პურფუნთუშეული</a></li>
+							</ul>
+						</li>
+						<li><a href="" aria-label="ვაკანსიები">ვაკანსიები</a></li>
+						<li><a href="" aria-label="სიახლეები">სიახლეები</a></li>
+						<li>
+							<a href="" aria-label="შესახებ">შესახებ</a>
+							<ul>
+								<li><a href="" aria-label="ჩვენ შესახებ">ჩვენ შესახებ</a></li>
+								<li><a href="" aria-label="ჩვენი გუნდი">ჩვენი გუნდი</a></li>
+							</ul>
+						</li>
+						<li><a href="" aria-label="გალერეა">გალერეა</a></li>
+						<li><a href="" aria-label="კონტაქტი">კონტაქტი</a></li>
+					</ul>
+				</nav>
+
+				<div class="languages">
+					<ul>
+						<li><a href="" class="active">KA</a></li>
+						<li><a href="">EN</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 </header>
 
 
@@ -35,12 +95,12 @@ $version = "1.0.1";
 
 
 
-<script type="text/javascript" src="/_markup/assets/js/jquery.js"></script>
-<script type="text/javascript" src="/_markup/assets/lightbox/jquery.lightbox.js"></script>
-<script type="text/javascript" src="/_markup/assets/bootstrap/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="/_markup/assets/js/owl.carousel.min.js"></script>
-<script type="text/javascript" src="/_markup/assets/js/main.js?v=<?=$version?>"></script>
-<script type="text/javascript" src="/_markup/assets/aos/aos.js"></script>
+<script src="/_markup/assets/js/jquery.js"></script>
+<script src="/_markup/assets/lightbox/jquery.lightbox.js"></script>
+<script src="/_markup/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="/_markup/assets/js/owl.carousel.min.js"></script>
+<script src="/_markup/assets/js/main.js?v=<?=$version?>"></script>
+<script src="/_markup/assets/aos/aos.js"></script>
 </body>
 
 </html>
