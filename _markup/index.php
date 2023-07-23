afe054546cf0eacf373474a64466b184e111caf1
giorgi <giorgigvazava87@gmail.com>
2023-07-23 15:35:51 +0400

    main section done

--- _markup/index.php
+++ _markup/index.php
@@ -15,8 +15,13 @@ $version = "1.0.1";
 	<link rel="stylesheet" type="text/css" href="/_markup/assets/aos/aos.css">
 	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/ge.css?v=<?=$version?>">
 	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/responsive_ge.css?v=<?=$version?>">
+
+	<link rel="preload" as="image" href="https://billion.404.ge/_markup/assets/img/leaf.svg">
+	<link rel="preload" as="image" href="https://billion.404.ge/_markup/assets/img/fruits-min.png">
+	<link rel="preload" as="image" href="https://billion.404.ge/_markup/assets/img/bag2-min.png">
+	<link rel="preload" as="image" href="https://billion.404.ge/_markup/assets/img/sprite.svg">
 </head>
-<body>
+<body class="home">
 
 <div class="mobile-navigation"></div>
 
@@ -86,7 +91,62 @@ $version = "1.0.1";
 
 
 <main>
-	
+	<section class="welcome">
+		<div class="leaf">leaf</div>
+		<div class="wave">wave</div>
+
+		<div class="center">
+			<div class="left">
+				<div class="slogan">
+					<span>გაიმარტივე ცხოვრება</span>
+				</div>
+
+				<h2>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია</h2>
+				<h1>საბეჭდი და ტიპოგრაფიულიინდ</h1>
+				<div class="clearfix"></div>
+				<button type="button" class="mainBtn" arial-label="ტიპოგრაფიოული"><span>ტიპოგრაფიული</span><em></em></button>
+
+				<div class="bubles">
+					<div class="buble b1"></div>
+					<div class="buble b2"></div>
+					<div class="buble b3"></div>
+					<div class="buble b4"></div>
+				</div>
+			</div>
+
+			<div class="right">
+				<div class="fruits">fruits</div>
+				<div class="bag">bag</div>
+
+				<div class="bubles">
+					<div class="buble b1"></div>
+					<div class="buble b2"></div>
+					<div class="buble b3"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="about-us">
+		<div class="center">
+			<h2>ჩვენ შესახებ</h2>
+
+			<div class="content">
+				<div class="image-box">
+					<div class="image" style="background-image:url('/_markup/assets/img/about.jpg')">
+						<img src="/_markup/assets/img/about.jpg" alt="" loading="lazy" class="imageHide" />	
+					</div>
+				</div>
+
+				<div class="text">
+					<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა ამწყობ დაზგაზე წიგნის საცდელი ეგზემპლარი დაბეჭდა. მისი ტექსტი არამარტო 5 საუკუნის მანძილზე შემორჩა, არამედ მან დღემდე, ელექტრონული ტიპოგრაფიის დრომდეც უცვლელად მოაღწია. განსაკუთრებული პოპულარობა მას 1960-იან წლებში გამოსულმა საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა ამწყობ დაზგაზე წიგნის საცდელი ეგზემპლარი დაბეჭდა. მისი ტექსტი არამარტო 5 საუკუნის მანძილზე შემორჩა, არამედ მან დღემდე, ელექტრონული ტიპოგრაფიის დრომდეც უცვლელად მოაღწია. განსაკუთრებული პოპულარობა მას 1960-იან წლებში გამოსულმა
+					საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა ამწყობ დაზგაზე წიგნის საცდელი ეგზემპლარი დაბეჭდა. მისი ტექსტი არამარტო 5</p>
+
+					<a href="" class="read-more" arial-label="სრულად ნახვა">სრულად ნახვა</a>
+				</div>
+			</div>
+		</div>
+	</section>
 </main>
 
 <footer>
