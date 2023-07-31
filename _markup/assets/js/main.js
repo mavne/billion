71929af69d20c93ccb6c596c19e93a40ef8929a3
giorgi <giorgigvazava87@gmail.com>
2023-07-31 12:22:07 +0400

    save yesterdays work

--- _markup/assets/js/main.js
+++ _markup/assets/js/main.js
@@ -29,12 +29,135 @@ class VisiableTracker{
 
 class Home{
 	welcomeSection(){
-		var height = $(document).height() - 116;
+		var height = $(window).height() - 116;
 		$('main').css('height', height+"px");
 	}
 
 	run(){
 		this.welcomeSection();
+	}
+}
+
+class About{
+	products(){
+		var containerWidth = $('.home main .about-us .center .content').width();
+		var oneItemWidth = (containerWidth / 10);
+
+		$('.home main .about-us .center .content .products .wrapper .item').css('flex-basis', oneItemWidth+"px");
+
+		var items = $('.home main .about-us .center .content .products .wrapper .item').length;
+		var fullWidth = (items * oneItemWidth) + 8;
+
+		$('.home main .about-us .center .content .products .wrapper').css('width', fullWidth+"px");
+		
+	}
+
+	makeProductDragable(){
+		let isMouseDown = false;
+		let startX;
+		let scrollLeft;
+
+		const scrollContainer = document.querySelector('.home main .about-us .center .content .products');
+
+		scrollContainer.addEventListener('mousedown', (e) => {
+		  isMouseDown = true;
+		  startX = e.pageX - scrollContainer.offsetLeft;
+		  scrollLeft = scrollContainer.scrollLeft;
+		});
+
+		scrollContainer.addEventListener('mouseup', () => {
+		  isMouseDown = false;
+		});
+
+		scrollContainer.addEventListener('mouseleave', () => {
+		  isMouseDown = false;
+		});
+
+		scrollContainer.addEventListener('mousemove', (e) => {
+		  if (!isMouseDown) return;
+		  e.preventDefault();
+		  const x = e.pageX - scrollContainer.offsetLeft;
+		  const walk = (x - startX) * 2;
+		  scrollContainer.scrollLeft = scrollLeft - walk;
+		});
+
+	}
+
+	productScrolls(){
+		setTimeout(function(){
+			$(document).on('click', '#scroll-left-button', function() {
+				$('#scroll-container').animate({
+			      scrollLeft: '-=270'
+			    }, 'slow');
+			});
+
+			$(document).on('click', '#scroll-right-button', function() {
+				$('#scroll-container').animate({
+			      scrollLeft: '+=270'
+			    }, 'slow');
+			});
+		}, 500);
+	}
+
+	run(){
+		this.products();
+		this.productScrolls();
+		this.makeProductDragable();
+	}
+}
+
+class News{
+	setLastActiveClass(owl) {
+      var activeItems = $('#owl-news-slider').trigger('refresh.owl.carousel').find('.owl-item.active');
+      var lastActive = activeItems.last().index();
+      $('#owl-news-slider .owl-item').removeClass('last-active');
+      $('#owl-news-slider .owl-item').eq(lastActive).addClass('last-active');
+    }
+
+	owlSlider(){
+		var that = this;
+		var owl = $('#owl-news-slider').owlCarousel({
+		    loop:false,
+		    margin:20,
+		    nav:false,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:1
+		        },
+		        1000:{
+		            items:4
+		        }
+		    },
+		    onDrag: function(){
+		    	setTimeout(function(){
+		    		that.setLastActiveClass($('#owl-news-slider'));	
+		    	}, 500);
+		    	
+		    }
+		});
+
+	    this.setLastActiveClass(owl);
+	}
+
+	addHoveredClass(){
+		$(document).on('mouseenter', '.home main .news .news-box #owl-news-slider .item a', function(){
+			$(this).addClass('hovered');
+		});
+	}
+
+	removeHoveredClass(){
+		$(document).on('mouseleave', '.home main .news .news-box #owl-news-slider .item a', function(){
+			$(this).removeClass('hovered');
+		});
+	}
+
+	run(){
+		this.owlSlider();
+		this.addHoveredClass();
+		this.removeHoveredClass();
 	}
 }
 
@@ -45,3 +168,11 @@ visiableTracker.run();
 /* Home */
 var home = new Home;
 home.run();
+
+/* About */
+var about = new About;
+about.run();
+
+/* News */
+var news = new News;
+news.run();
--- _markup/index.php
+++ _markup/index.php
@@ -1,5 +1,5 @@
 <?php 
-$version = "1.0.1";
+$version = "1.0.4";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,6 +20,55 @@ $version = "1.0.1";
 	<link rel="preload" as="image" href="https://billion.404.ge/_markup/assets/img/fruits-min.png">
 	<link rel="preload" as="image" href="https://billion.404.ge/_markup/assets/img/bag2-min.png">
 	<link rel="preload" as="image" href="https://billion.404.ge/_markup/assets/img/sprite.svg">
+
+
+<script>
+    var map, markers, myLatLng;
+    var locations = [
+        { lat: 41.6329098036272, lng: 41.618115356584624 },
+        { lat: 41.63791340938079, lng: 41.629273345180316 },
+        { lat: 41.650677259216636, lng: 41.6384572281014 },
+    ];
+
+    function googleMap() {
+    	var mapStyle = [{"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape.natural","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural.landcover","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#ffffff"},{"visibility":"on"}]}];
+        
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: locations[0], 
+            zoom: 12,
+            scrollwheel: false,
+            disableDefaultUI: true,
+            zoomControl: true,
+            styles: mapStyle
+        });
+
+        markers = locations.map(location => {
+            return new google.maps.Marker({
+                position: location,
+                map: map,
+                icon: '/_markup/assets/img/marker.svg',
+                animation: google.maps.Animation.DROP,
+                draggable: false
+            });
+        });
+    }
+
+    function initMaps() {
+        if (window.ggMaps) {
+            return false;
+        }
+        window.ggMaps = true;
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCfLSXv1i5iAG3WdJzqWsVOaFddXAxiDUQ&callback=googleMap';
+        document.head.appendChild(script);
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        // setTimeout(initMaps, 3500);
+    });
+</script>
 </head>
 <body class="home">
 
@@ -104,7 +153,7 @@ $version = "1.0.1";
 				<h2>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია</h2>
 				<h1>საბეჭდი და ტიპოგრაფიულიინდ</h1>
 				<div class="clearfix"></div>
-				<button type="button" class="mainBtn" arial-label="ტიპოგრაფიოული"><span>ტიპოგრაფიული</span><em></em></button>
+				<button type="button" class="mainBtn"><span>ტიპოგრაფიული</span><em></em></button>
 
 				<div class="bubles">
 					<div class="buble b1"></div>
@@ -129,12 +178,12 @@ $version = "1.0.1";
 
 	<section class="about-us">
 		<div class="center">
-			<h2>ჩვენ შესახებ</h2>
+			<h2 class="sec-title">ჩვენ შესახებ</h2>
 
 			<div class="content">
 				<div class="image-box">
 					<div class="image" style="background-image:url('/_markup/assets/img/about.jpg')">
-						<img src="/_markup/assets/img/about.jpg" alt="" loading="lazy" class="imageHide" />	
+						<img src="/_markup/assets/img/about.jpg" alt="" loading="lazy" class="imageHide">	
 					</div>
 				</div>
 
@@ -142,9 +191,359 @@ $version = "1.0.1";
 					<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა ამწყობ დაზგაზე წიგნის საცდელი ეგზემპლარი დაბეჭდა. მისი ტექსტი არამარტო 5 საუკუნის მანძილზე შემორჩა, არამედ მან დღემდე, ელექტრონული ტიპოგრაფიის დრომდეც უცვლელად მოაღწია. განსაკუთრებული პოპულარობა მას 1960-იან წლებში გამოსულმა საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა ამწყობ დაზგაზე წიგნის საცდელი ეგზემპლარი დაბეჭდა. მისი ტექსტი არამარტო 5 საუკუნის მანძილზე შემორჩა, არამედ მან დღემდე, ელექტრონული ტიპოგრაფიის დრომდეც უცვლელად მოაღწია. განსაკუთრებული პოპულარობა მას 1960-იან წლებში გამოსულმა
 					საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა ამწყობ დაზგაზე წიგნის საცდელი ეგზემპლარი დაბეჭდა. მისი ტექსტი არამარტო 5</p>
 
-					<a href="" class="read-more" arial-label="სრულად ნახვა">სრულად ნახვა</a>
+					<a href="" class="read-more">სრულად ნახვა</a>
+				</div>
+
+				<div class="clearfix"></div>
+
+				<div class="products" id="scroll-container">
+					<div class="wrapper">
+						<div class="item">
+							<div class="box">
+								<div class="icon icon1"></div>
+								<p>ბილიონის წარმოება</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon2"></div>
+								<p>ხილ-ბოსტნეული</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon3"></div>
+								<p>სასმელი</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon4"></div>
+								<p>ჩაი ყავა კაკაო</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon5"></div>
+								<p>პურ ფუნთუშეული</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon6"></div>
+								<p>ტკბილეული ნაყინი</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon7"></div>
+								<p>სნექი</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon8"></div>
+								<p>კონსერვი</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon9"></div>
+								<p>სოსისი, ძეხვეული</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon10"></div>
+								<p>ხორცი, ნახევარ-ფაბრიკატები</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon11"></div>
+								<p>კვერცხი, რძის ნაწარმი</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon12"></div>
+								<p>ზღვის პროდუქტები</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon13"></div>
+								<p>სახლის მოვლა, ჰიგიენა</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon14"></div>
+								<p>ცხოველთა კვება</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon15"></div>
+								<p>საკანცელარიო, ჭურჭელი</p>
+							</div>
+						</div>
+
+						<div class="item">
+							<div class="box">
+								<div class="icon icon16"></div>
+								<p>სანტექნიკა</p>
+							</div>
+						</div>						
+					</div>
+				</div>
+
+				<div class="buttons">
+					<a href="javascript:void(0)" arial-label="prev" id="scroll-left-button">prev</a>
+					<a href="javascript:void(0)" arial-label="next" id="scroll-right-button">next</a>
+				</div>
+
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</section>
+
+	<section class="market-map">
+		<div class="center">
+			<h2 class="sec-title">მარკეტების რუკა</h2>
+
+			<div class="map-box">
+				<div class="addresses">
+					<ul>
+						<li><a href="">ბათუმი, ჭავჭავაძის 80</a></li>
+						<li><a href="">ბათუმი, ჭავჭავაძის 80</a></li>
+						<li><a href="">ბათუმი, ჭავჭავაძის 80</a></li>
+						<li><a href="">ბათუმი, ჭავჭავაძის 80</a></li>
+						<li style="opacity:0.8"><a href="">ბათუმი, ჭავჭავაძის 80</a></li>
+						<li style="opacity:0.4"><a href="">ბათუმი, ჭავჭავაძის 80</a></li>
+						<li style="opacity:0.2"><a href="">ბათუმი, ჭავჭავაძის 80</a></li>
+					</ul>
+
+					<a href="" class="see-on-map">რუკაზე ნახვა</a>
+				</div>
+
+				<div class="right">
+					<div class="map" id="map" style="background-image:url('/_markup/assets/img/theMap.jpg')">map</div>
+				</div>
+
+				<div class="clearer"></div>
+			</div>
+		</div>
+	</section>
+
+	<section class="vacancy">
+		<div class="wave"></div>
+		<div class="center">
+			<div class="right">
+				<h2>ბილიონში ვაკანსიები გვაქ!</h2>
+				<a href="" class="employMe"><span>მსურს დასაქმება</span><em></em></a>
+			</div>
+
+			<div class="clearfix"></div>
+		</div>
+	</section>
+
+	<section class="news">
+		<div class="center">
+			<h2 class="sec-title">სიახლეები</h2>
+
+			<div class="news-box">
+				<div class="owl-carousel owl-theme" id="owl-news-slider">
+					<div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
+
+				    <div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
+
+				    <div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
+
+				    <div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
+
+				    <div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
+
+				    <div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
+
+				    <div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
+
+				    <div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
+
+
+				    <div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
+
+				    <div class="item">
+				    	<a href="" class="content">
+				    		<div class="image-box">
+				    			<img src="/_markup/assets/img/news.jpg" alt="" loading="lazy">
+				    			<em class="cover"></em>
+				    			<span>12.12.2023</span>
+				    		</div>
+
+				    		<div class="text">
+				    			<h3>სიახლის სათაური</h3>
+
+				    			<p>საბეჭდი და ტიპოგრაფიული ინდუსტრიის უშინაარსო ტექსტია. იგი სტანდარტად 1500-იანი წლებიდან იქცა, როდესაც უცნობმა მბეჭდავმა </p>
+				    		</div>
+				    		<div class="button-arrow" arial-label="read more"></div>
+				    	</a>
+				    </div>
 				</div>
 			</div>
+
+			<div class="clearfix"></div>
+			<a href=""><span>ყველა სიახლის ნახვა</span><em></em></a>
 		</div>
 	</section>
 </main>
@@ -159,7 +558,7 @@ $version = "1.0.1";
 <script src="/_markup/assets/lightbox/jquery.lightbox.js"></script>
 <script src="/_markup/assets/bootstrap/js/bootstrap.min.js"></script>
 <script src="/_markup/assets/js/owl.carousel.min.js"></script>
-<script src="/_markup/assets/js/main.js?v=<?=$version?>"></script>
+<script src="/_markup/assets/js/compressed.js?v=<?=$version?>"></script>
 <script src="/_markup/assets/aos/aos.js"></script>
 </body>
 
