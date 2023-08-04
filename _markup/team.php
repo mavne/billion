e4000ea4f7ae26e88bc6e67218830bd0992c5935
giorgi <giorgigvazava87@gmail.com>
2023-08-04 19:11:24 +0400

    save

---
+++ _markup/team.php
@@ -0,0 +1,160 @@
+<?php 
+$version = "1.0.6";
+?>
+<!DOCTYPE html>
+<html lang="ka">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="/_markup/assets/img/favicon.svg" type="image/svg" sizes="64x64">
+	<title>Billion</title>
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/owl/owl.carousel.min.css">
+	<link rel="stylesheet" href="/_markup/assets/lightbox/jquery.lightbox.css">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/main.css?v=<?=$version?>">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/aos/aos.css">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/ge.css?v=<?=$version?>">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/responsive_ge.css?v=<?=$version?>">
+
+
+	<link rel="preload" as="image" crossorigin="anonymous" href="https://billion.404.ge/_markup/assets/img/sprite.svg">
+	<link rel="preload" as="image" crossorigin="anonymous" href="https://billion.404.ge/_markup/assets/img/inner.top.wave.svg">
+	<link rel="preload" as="image" crossorigin="anonymous" href="https://billion.404.ge/_markup/assets/img/footer.wave.svg">
+	<link rel="preload" as="image" crossorigin="anonymous" href="https://billion.404.ge/_markup/assets/img/foods.bg.svg">
+</head>
+<body class="inner-page team-page">
+
+<div class="mobile-navigation"></div>
+
+<header>
+	<div class="top">
+		<div class="center">
+			<div class="content">
+				<ul class="social">
+					<li><a href="" class="facebook" aria-label="Facebook">facebook</a></li>
+					<li><a href="" class="instagram" aria-label="Instagram">instagram</a></li>
+					<li><a href="" class="youtube" aria-label="Youtube">youtube</a></li>
+				</ul>
+
+				<div class="line"></div>
+
+				<div class="phone">
+					<a href="" aria-label="Contact number" class="call">
+						<span>032 215 15 09</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="bottom">
+		<div class="center">
+			<a href="" class="logo" aria-label="Logo">Logo</a>
+
+			<div class="right">
+				<nav>
+					<ul>
+						<li><a href="" aria-label="მარკეტების რუკა" class="active">მარკეტების რუკა</a></li>
+						<li><a href="" aria-label="ფასდაკლებები">ფასდაკლებები</a></li>
+						<li>
+							<a href="" aria-label="პროდუქტები">პროდუქტები</a>
+							<ul>
+								<li><a href="" aria-label="ხილ-ბოსტნეული">ხილ-ბოსტნეული</a></li>
+								<li><a href="" aria-label="სასმელი">სასმელი</a></li>
+								<li><a href="" aria-label="ჩაი ყავა კაკაო">ჩაი ყავა კაკაო</a></li>
+								<li><a href="" aria-label="პურფუნთუშეული">პურფუნთუშეული</a></li>
+							</ul>
+						</li>
+						<li><a href="" aria-label="ვაკანსიები">ვაკანსიები</a></li>
+						<li><a href="" aria-label="სიახლეები">სიახლეები</a></li>
+						<li>
+							<a href="" aria-label="შესახებ">შესახებ</a>
+							<ul>
+								<li><a href="" aria-label="ჩვენ შესახებ">ჩვენ შესახებ</a></li>
+								<li><a href="" aria-label="ჩვენი გუნდი">ჩვენი გუნდი</a></li>
+							</ul>
+						</li>
+						<li><a href="" aria-label="გალერეა">გალერეა</a></li>
+						<li><a href="" aria-label="კონტაქტი">კონტაქტი</a></li>
+					</ul>
+				</nav>
+
+				<div class="languages">
+					<ul>
+						<li><a href="" class="active">KA</a></li>
+						<li><a href="">EN</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+
+<main>
+	<section class="content-box">
+		<div class="center">
+			<h1 class="sec-title">ჩვენი გუნდი</h1>
+
+			
+		</div>
+	</section>
+</main>
+
+<footer>
+	<div class="center">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="fdata">
+							<h3>ოფისი</h3>
+							<p>გორგილაძის N3 , ბათუმი , საქართველო</p>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="fdata">
+							<h3>მეილი</h3>
+							<p><a href="">billion@gmail.com</a></p>
+							<p><a href="">billionvacancy@gmail.com</a></p>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="fdata">
+							<h3>ტელეფონი</h3>
+							<p><a href="">+995 555 123 345</a></p>
+							<p><a href="">+995 555 123 345</a></p>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<h3>გამოგვყევი</h3>
+						<div class="fdata">
+							<ul>
+								<li><a href="" class="facebook">facebook</a></li>
+								<li><a href="" class="instagram">instagram</a></li>
+								<li><a href="" class="youtube">youtube</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="copyright">
+			<p>ყველა უფლება დაცულია ©2023</p>
+			<a href="https://shindi.ge/" class="shindi" target="_blank">shindi</a>
+		</div>
+	</div>
+</footer>
+
+<script src="/_markup/assets/js/jquery.js"></script>
+<script src="/_markup/assets/lightbox/jquery.lightbox.js"></script>
+<script src="/_markup/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="/_markup/assets/js/owl.carousel.min.js"></script>
+<script src="/_markup/assets/js/compressed.js?v=<?=$version?>"></script>
+<script src="/_markup/assets/aos/aos.js"></script>
+</body>
+</html>
